09d89b87e6f6bc183da6e9741d3556d986a9abb5
Logic1ne <selotan@gmx.de>
2018-06-22 20:02:10 +0200

    Add files via upload

--- whaleSiam/applywhale.py
+++ whaleSiam/applywhale.py
@@ -1,4 +1,13 @@
 import torch
+try:
+    torch._utils._rebuild_tensor_v2
+except AttributeError:
+    def _rebuild_tensor_v2(storage, storage_offset, size, stride, requires_grad, backward_hooks):
+        tensor = torch._utils._rebuild_tensor(storage, storage_offset, size, stride)
+        tensor.requires_grad = requires_grad
+        tensor._backward_hooks = backward_hooks
+        return tensor
+    torch._utils._rebuild_tensor_v2 = _rebuild_tensor_v2
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -15,6 +24,7 @@ from trainWhalesSiam import Net
 import pandas as pd
 import os
 import numpy as np
+import gc
 root_dir = "./test"
 csv_file = ('./oneshot.csv')
 labels_frame1 = pd.read_csv(csv_file)
@@ -25,7 +35,7 @@ normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
 
 net = Net()
 #net = net.cuda()
-net.load_state_dict(torch.load("./model10.pth"))
+net.load_state_dict(torch.load("./model.pth"))
 test_transf = Compose([ToPILImage(), Resize(img_size), ToTensor(), normalize])
 result = []
 for i in range(len(labels_frame2)):
@@ -47,17 +57,29 @@ for i in range(len(labels_frame2)):
 		#image2 = image2.cuda()
 		out1, out2 = net(image1, image2)
 		euclidean_distance = F.pairwise_distance(out1, out2)
-		dists.append([j, euclidean_distance])
+		#print(euclidean_distance.data[0][0])
+		dists.append([j, euclidean_distance.data[0][0]])
+		del out1, out2, image2, img_name2, euclidean_distance
+		torch.cuda.empty_cache()
+		gc.collect()
+		#if(j == 5):
+
+		#	break
 		if(j%100 == 0):
 			print(j)
 
 	dists.sort(key=lambda dist: dist[1])
-	print("i: {}, img_name1: {}, top1: {}".format(i, img_name1, str(labels_frame1.iloc[dists[0,0],1])))
-	result.append([img_name1, "{} {} {} {} {}".format(str(labels_frame1.iloc[dists[0,0],1]), 
-    												str(labels_frame1.iloc[dists[1,0],1]), 
-    												str(labels_frame1.iloc[dists[2,0],1]), 
-    												str(labels_frame1.iloc[dists[3,0],1]), 
-    												str(labels_frame1.iloc[dists[4,0],1]))])
+	print("i: {}, img_name1: {}, top1: {}".format(i, labels_frame2.iloc[i,0], str(labels_frame1.iloc[dists[0][0],1])))
+	result.append([labels_frame2.iloc[i,0], "{} {} {} {} {}".format(str(labels_frame1.iloc[dists[0][0],1]), 
+    												str(labels_frame1.iloc[dists[1][0],1]), 
+    												str(labels_frame1.iloc[dists[2][0],1]), 
+    												str(labels_frame1.iloc[dists[3][0],1]), 
+    												str(labels_frame1.iloc[dists[4][0],1]))])
+	#print(result)
+	#exit()
+	del image1
+	gc.collect()
+	torch.cuda.empty_cache()
 
 
 
